f48486f7503e548c992a2af3235838a9f8143e48
waterproofpatch <waterproofpatch@gmail.com>
2020-02-15 11:57:29 -0500

    Test template

---
+++ test/test_template.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "unity.h"
+
+// mocks
+#include "mock_std_lib_wrapper.h"
+
+// code under test
+#include "list.h"
+
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+void testABC()
+{
+    TEST_ASSERT_EQUAL(1, 1);
+}
+
