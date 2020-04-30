8ababfd9a77974324009410dfe962103e8dfad9f
waterproofpatch <waterproofpatch@gmail.com>
2020-04-30 00:33:37 -0400

    some more tests

--- test/test_list.c
+++ test/test_list.c
@@ -22,35 +22,40 @@ void tearDown()
     TEST_ASSERT_EQUAL(0, g_allocs - g_frees);
 }
 
-void proc_func(void* element, void* context)
+void proc_func_assert_null(void *element, void *context)
+{
+    TEST_ASSERT_NULL(context);
+}
+
+void proc_func(void *element, void *context)
 {
     static int num_proc_calls;
     num_proc_calls++;
-    TEST_ASSERT_EQUAL(101, *(int*)context);
-    TEST_ASSERT_EQUAL(num_proc_calls, *(int*)element);
+    TEST_ASSERT_EQUAL(101, *(int *)context);
+    TEST_ASSERT_EQUAL(num_proc_calls, *(int *)element);
 }
 
-char comparator(void* e1, void* e2)
+char comparator(void *e1, void *e2)
 {
     // neither of these should be NULL
     TEST_ASSERT_NOT_NULL(e1);
     TEST_ASSERT_NOT_NULL(e2);
 
     // we know the type is int
-    int e1_val = *(int*)e1;
-    int e2_val = *(int*)e2;
+    int e1_val = *(int *)e1;
+    int e2_val = *(int *)e2;
 
     // return the diff
     return (e2_val - e1_val) == 0;
 }
 
-void* wrap_malloc_stub(size_t size, int count)
+void *wrap_malloc_stub(size_t size, int count)
 {
     g_allocs++;
     return malloc(size);
 }
 
-void wrap_free_stub(void* ptr, int count)
+void wrap_free_stub(void *ptr, int count)
 {
     g_frees++;
     return free(ptr);
@@ -59,21 +64,31 @@ void wrap_free_stub(void* ptr, int count)
 void testListInitNoMem()
 {
     wrap_malloc_ExpectAndReturn(sizeof(list_t), NULL);
-    list_t* list = list_init(wrap_malloc, wrap_free);
+    list_t *list = list_init(wrap_malloc, wrap_free);
     TEST_ASSERT_NULL(list);
 }
 
 void testListInit()
 {
-    list_t* ret_list = malloc(sizeof(list_t));
+    list_t *ret_list = malloc(sizeof(list_t));
     wrap_malloc_ExpectAndReturn(sizeof(list_t), ret_list);
     wrap_free_Expect(ret_list);
 
-    list_t* list = list_init(wrap_malloc, wrap_free);
+    list_t *list = list_init(wrap_malloc, wrap_free);
     TEST_ASSERT_NOT_NULL(list);
 
     free(ret_list);
     list_destroy(list);
+}
+
+void testListInitNullMalloc()
+{
+    TEST_ASSERT_NULL(list_init(NULL, wrap_free));
+}
+
+void testListInitNullFree()
+{
+    TEST_ASSERT_NULL(list_init(wrap_malloc, NULL));
 }
 
 void testListRemoveHead()
@@ -81,7 +96,7 @@ void testListRemoveHead()
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
 
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
     TEST_ASSERT_EQUAL(1, list_remove(list, &elem_1));
@@ -93,7 +108,7 @@ void testListCount()
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
 
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     int     elem_2 = 2;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
@@ -110,12 +125,12 @@ void testListAddRemoveAdd()
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
 
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
     TEST_ASSERT_EQUAL(1, list_remove(list, &elem_1));
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
-    void* res = list_get_at_index(list, 0);
+    void *res = list_get_at_index(list, 0);
     TEST_ASSERT_EQUAL(&elem_1, res);
     TEST_ASSERT_EQUAL(1, list_remove(list, &elem_1));
     res = list_get_at_index(list, 0);
@@ -128,7 +143,7 @@ void testListForEach()
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
 
-    list_t* list    = list_init(wrap_malloc, wrap_free);
+    list_t *list    = list_init(wrap_malloc, wrap_free);
     int     elem_1  = 1;
     int     elem_2  = 2;
     int     elem_3  = 3;
@@ -142,12 +157,31 @@ void testListForEach()
     list_destroy(list);
 }
 
+void testListForEachNullContext()
+{
+    wrap_malloc_StubWithCallback(wrap_malloc_stub);
+    wrap_free_StubWithCallback(wrap_free_stub);
+
+    list_t *list    = list_init(wrap_malloc, wrap_free);
+    int     elem_1  = 1;
+    int     elem_2  = 2;
+    int     elem_3  = 3;
+    int     elem_4  = 4;
+    int     context = 101;
+    TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
+    TEST_ASSERT_EQUAL(1, list_add(list, &elem_2));
+    TEST_ASSERT_EQUAL(1, list_add(list, &elem_3));
+    TEST_ASSERT_EQUAL(1, list_add(list, &elem_4));
+    list_foreach(list, proc_func_assert_null, NULL);
+    list_destroy(list);
+}
+
 void testListAddAndRemoveSingleElement()
 {
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
 
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
     TEST_ASSERT_EQUAL(1, list->count);
@@ -164,7 +198,7 @@ void testListRemoveElemNotExist()
     wrap_free_StubWithCallback(wrap_free_stub);
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
 
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     int     elem_2 = 2;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
@@ -175,7 +209,7 @@ void testListRemoveSecondElement()
 {
     wrap_free_StubWithCallback(wrap_free_stub);
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     int     elem_2 = 2;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
@@ -186,12 +220,12 @@ void testListRemoveSecondElement()
 
 void testListAddNoMemory()
 {
-    list_t* list_alloc = (list_t*)malloc(sizeof(list_t));
+    list_t *list_alloc = (list_t *)malloc(sizeof(list_t));
     g_allocs++;   // since we're making this allocation outside the wrapper
     wrap_free_StubWithCallback(wrap_free_stub);
     wrap_malloc_ExpectAndReturn(sizeof(list_t), list_alloc);
     wrap_malloc_ExpectAndReturn(sizeof(list_node_t), NULL);
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     TEST_ASSERT_EQUAL(0, list_add(list, &elem_1));
     list_destroy(list);
@@ -201,7 +235,7 @@ void testListAddTwoElementsAndDestroy()
 {
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     int     elem_2 = 2;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
@@ -214,13 +248,13 @@ void testListGetAtIndex()
 {
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     int     elem_2 = 2;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_2));
-    int elem_at_index_1 = *(int*)list_get_at_index(list, 0);
-    int elem_at_index_2 = *(int*)list_get_at_index(list, 1);
+    int elem_at_index_1 = *(int *)list_get_at_index(list, 0);
+    int elem_at_index_2 = *(int *)list_get_at_index(list, 1);
     TEST_ASSERT_EQUAL(1, elem_at_index_1);
     TEST_ASSERT_EQUAL(2, elem_at_index_2);
     TEST_ASSERT_EQUAL(NULL, list_get_at_index(list, 2));
@@ -231,9 +265,9 @@ void testListSearchNoElements()
 {
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
-    list_t* list = list_init(wrap_malloc, wrap_free);
+    list_t *list = list_init(wrap_malloc, wrap_free);
     int     key  = 1;
-    void*   res  = list_search(list, comparator, &key);
+    void *  res  = list_search(list, comparator, &key);
     TEST_ASSERT_NULL(res);
     list_destroy(list);
 }
@@ -242,13 +276,13 @@ void testListSearchOneElement()
 {
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     TEST_ASSERT_EQUAL(1, list_add(list, &elem_1));
     int   key = 1;
-    void* res = list_search(list, comparator, &key);
+    void *res = list_search(list, comparator, &key);
     TEST_ASSERT_NOT_NULL(res);
-    TEST_ASSERT_EQUAL(1, *(int*)res);
+    TEST_ASSERT_EQUAL(1, *(int *)res);
     list_destroy(list);
 }
 
@@ -256,7 +290,7 @@ void testListSearchThreeElements()
 {
     wrap_malloc_StubWithCallback(wrap_malloc_stub);
     wrap_free_StubWithCallback(wrap_free_stub);
-    list_t* list   = list_init(wrap_malloc, wrap_free);
+    list_t *list   = list_init(wrap_malloc, wrap_free);
     int     elem_1 = 1;
     int     elem_2 = 2;
     int     elem_3 = 3;
@@ -267,15 +301,15 @@ void testListSearchThreeElements()
     int   key_1 = 1;
     int   key_2 = 2;
     int   key_3 = 3;
-    void* res_1 = list_search(list, comparator, &key_1);
-    void* res_2 = list_search(list, comparator, &key_2);
-    void* res_3 = list_search(list, comparator, &key_3);
+    void *res_1 = list_search(list, comparator, &key_1);
+    void *res_2 = list_search(list, comparator, &key_2);
+    void *res_3 = list_search(list, comparator, &key_3);
     TEST_ASSERT_NOT_NULL(res_1);
-    TEST_ASSERT_EQUAL(1, *(int*)res_1);
+    TEST_ASSERT_EQUAL(1, *(int *)res_1);
     TEST_ASSERT_NOT_NULL(res_2);
-    TEST_ASSERT_EQUAL(2, *(int*)res_2);
+    TEST_ASSERT_EQUAL(2, *(int *)res_2);
     TEST_ASSERT_NOT_NULL(res_3);
-    TEST_ASSERT_EQUAL(3, *(int*)res_3);
+    TEST_ASSERT_EQUAL(3, *(int *)res_3);
 
     list_destroy(list);
 }
