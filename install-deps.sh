338fa824ccaab5e7ec6e9c44247aa83603b06825
waterproofpatch <waterproofpatch@gmail.com>
2020-05-08 18:18:54 -0400

    remove ;

--- install-deps.sh
+++
@@ -1,9 +0,0 @@
-# Need this for travis automated builds
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install python3-pip -y
-sudo apt-get install doxygen -y
-# sudo apt-get install ruby -y
-sudo apt-get install gcc -y
-sudo pip3 install gcovr
-sudo gem install ceedling
