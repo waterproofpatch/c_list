1ad5e2ac3b0f394a8fd2f3d251809451137ac0ef
waterproofpatch <waterproofpatch@gmail.com>
2020-05-08 18:12:48 -0400

    no install ruby here

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install python3-pip -y
 sudo apt-get install doxygen -y
-sudo apt-get install ruby -y
-sudo pip3 install gcovr
+# sudo apt-get install ruby -y
 sudo apt-get install gcc -y
+sudo pip3 install gcovr
 sudo gem install ceedling
