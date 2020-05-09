7af5b7629a61557585642cbb99f3b4efdbc17660
waterproofpatch <waterproofpatch@gmail.com>
2020-05-09 13:56:27 -0400

    refactor

--- test/test_list_remove.c
+++ test/test_list_remove.c
@@ -87,24 +87,18 @@ void test_list_remove_head()
  * */
 void test_list_remove_middle()
 {
-    list_node_t node_1 = {0};
-    list_node_t node_2 = {0};
-    list_node_t node_3 = {0};
-    list_t      list   = {0};
-
-    node_1.element = (void *)1;
-    node_1.next    = &node_2;
-    node_2.element = (void *)2;
-    node_2.next    = &node_3;
-    node_3.element = (void *)3;
-    node_3.next    = NULL;
-
-    list.head      = &node_1;
-    list.tail      = &node_3;
-    list.count     = 3;
-    list.list_free = stub_list_free;
+    /* Locals */
+    list_node_t node_3 = {.next = NULL, .element = (void *)3};
+    list_node_t node_2 = {.next = &node_3, .element = (void *)2};
+    list_node_t node_1 = {.next = &node_2, .element = (void *)1};
+    list_t      list   = {.head      = &node_1,
+                   .tail      = &node_3,
+                   .count     = 3,
+                   .list_free = stub_list_free};
 
     g_expected = &node_2; /* the middle */
+
+    /* Invoke code under test */
     TEST_ASSERT_EQUAL(1, list_remove(&list, node_2.element));
     TEST_ASSERT_EQUAL(2, list.count);
     TEST_ASSERT_EQUAL(&node_1, list.head);
@@ -112,47 +106,35 @@ void test_list_remove_middle()
     TEST_ASSERT_EQUAL(1, g_num_frees);
 }
 
-/**
- * @brief test that we can remove an item in the middle of the list
- * */
 void test_list_remove_does_not_exist()
 {
-    list_node_t node_1 = {0};
-    list_node_t node_2 = {0};
-    list_node_t node_3 = {0};
-    list_t      list   = {0};
+    /* Locals */
+    list_node_t node_3 = {.next = NULL, .element = (void *)3};
+    list_node_t node_2 = {.next = NULL, .element = (void *)2};
+    list_node_t node_1 = {.next = &node_3, .element = (void *)1};
+    list_t      list   = {.head      = &node_1,
+                   .tail      = &node_3,
+                   .count     = 2,
+                   .list_free = stub_list_free};
 
-    node_1.element = (void *)1;
-    node_1.next    = &node_3;
-    node_2.element = (void *)2;
-    node_2.next    = NULL;
-    node_3.element = (void *)3;
-    node_3.next    = NULL;
-
-    list.head      = &node_1;
-    list.tail      = &node_3;
-    list.count     = 2;
-    list.list_free = stub_list_free;
-
+    /* Invoke code under test */
     TEST_ASSERT_EQUAL(0, list_remove(&list, node_2.element));
+
+    /* Assertions */
     TEST_ASSERT_EQUAL(2, list.count);
     TEST_ASSERT_EQUAL(&node_1, list.head);
     TEST_ASSERT_EQUAL(&node_3, list.tail);
     TEST_ASSERT_EQUAL(0, g_num_frees);
 }
 
-/**
- * @brief test that the list handles invalid arguments
- * */
 void test_list_remove_fail_invalid_element()
 {
+    /* Invoke code under test */
     TEST_ASSERT_EQUAL(0, list_remove((list_t *)0xdeadbeef, NULL));
 }
 
-/**
- * @brief test that the list handles invalid arguments
- * */
 void test_list_remove_fail_invalid_list()
 {
+    /* Invoke code under test */
     TEST_ASSERT_EQUAL(0, list_remove(NULL, (void *)0xdeadbeef));
 }
