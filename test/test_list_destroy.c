9109fc8c094e6083224d8882259a8471f7cd6ef9
waterproofpatch <waterproofpatch@gmail.com>
2020-05-09 13:23:01 -0400

    refactor

--- test/test_list_destroy.c
+++ test/test_list_destroy.c
@@ -18,6 +18,11 @@ void tearDown()
 {
 }
 
+/**
+ * @brief verify that free is called with the correct element
+ *
+ * @param ptr to element to free
+ */
 void stub_free(void *ptr)
 {
     TEST_ASSERT_NOT_NULL(ptr);
@@ -26,47 +31,62 @@ void stub_free(void *ptr)
 
 void test_list_destroy_success()
 {
-    list_t list = {.list_malloc = (void (*)(size_t))0xdeadbeef,
+    /* Locals */
+    list_t list = {.list_malloc = (void *(*)(size_t))0xdeadbeef,
                    .list_free   = stub_free,
                    .head        = NULL,
                    .tail        = NULL,
                    .count       = 0};
+
+    /* Invoke function under test */
     list_destroy(&list);
+
+    /* Assertions */
     TEST_ASSERT_EQUAL(1, g_num_frees);
     TEST_ASSERT_EQUAL(0, list.count);
 }
 
 void test_list_destroy_success_one_entry()
 {
+    /* Locals */
     list_node_t entry_1 = {.element = (void *)0xdeadbeef, .next = NULL};
-
-    list_t list = {.list_malloc = (void (*)(size_t))0xdeadbeef,
+    list_t      list    = {.list_malloc = (void *(*)(size_t))0xdeadbeef,
                    .list_free   = stub_free,
                    .head        = &entry_1,
                    .tail        = &entry_1,
                    .count       = 1};
 
+    /* Invoke function under test */
     list_destroy(&list);
+
+    /* Assertions */
     TEST_ASSERT_EQUAL(2, g_num_frees);
 }
 
 void test_list_destroy_success_two_entries()
 {
+    /* Locals */
     list_node_t entry_1 = {.element = (void *)0xdeadbeef, .next = NULL};
     list_node_t entry_2 = {.element = (void *)0xfeedface, .next = &entry_1};
-
-    list_t list = {.list_malloc = (void (*)(size_t))0xdeadbeef,
+    list_t      list    = {.list_malloc = (void *(*)(size_t))0xdeadbeef,
                    .list_free   = stub_free,
                    .head        = &entry_2,
                    .tail        = &entry_1,
                    .count       = 1};
 
+    /* Invoke function under test */
     list_destroy(&list);
+
+    /* Assertions */
     TEST_ASSERT_EQUAL(3, g_num_frees);
 }
 
 void test_list_destroy_fail_invalid_list()
 {
+    /* Locals */
+    /* Invoke function under test */
     list_destroy(NULL);
+
+    /* Assertions */
     TEST_ASSERT_EQUAL(0, g_num_frees);
 }
