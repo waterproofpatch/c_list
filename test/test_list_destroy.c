28b4fab11da1ff1819928eab862bb90e93149574
waterproofpatch <waterproofpatch@gmail.com>
2020-05-09 12:08:13 -0400

    more tests

--- test/test_list_destroy.c
+++ test/test_list_destroy.c
@@ -43,12 +43,6 @@ void test_list_destroy_success()
     TEST_ASSERT_EQUAL(0, list.count);
 }
 
-void test_list_destroy_fail_invalid_list()
-{
-    list_destroy(NULL);
-    TEST_ASSERT_EQUAL(0, g_num_frees);
-}
-
 void test_list_destroy_success_one_entry()
 {
     list_node_t entry_1 = {.element = (void *)0xdeadbeef, .next = NULL};
@@ -61,4 +55,25 @@ void test_list_destroy_success_one_entry()
 
     list_destroy(&list);
     TEST_ASSERT_EQUAL(2, g_num_frees);
+}
+
+void test_list_destroy_success_two_entries()
+{
+    list_node_t entry_1 = {.element = (void *)0xdeadbeef, .next = NULL};
+    list_node_t entry_2 = {.element = (void *)0xfeedface, .next = &entry_1};
+
+    list_t list = {.list_malloc = stub_malloc,
+                   .list_free   = stub_free,
+                   .head        = &entry_2,
+                   .tail        = &entry_1,
+                   .count       = 1};
+
+    list_destroy(&list);
+    TEST_ASSERT_EQUAL(3, g_num_frees);
+}
+
+void test_list_destroy_fail_invalid_list()
+{
+    list_destroy(NULL);
+    TEST_ASSERT_EQUAL(0, g_num_frees);
 }
